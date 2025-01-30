10c3692a48f04f18162dcadf31d1e921c6f74cf6
EricMannikarottu <ericmannikarottu20@gmail.com>
2025-01-30 17:19:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+const dirPath="C:\\Users\\Eric Mannikarottu\\Desktop\\coding\\WD\\File management system"
+
+let files= await fs.readdir(dirPath)
+
+
+for (const item of files) {
+    const filePath= path.join(dirPath,item)
+    const ext=path.extname(item).slice(1)
+
+
+    if(ext ==="js"||ext === "json" ){
+        continue
+    }
+
+    console.log(ext)
+
+    const stats = await fs.stat(filePath)
+
+    if(stats.isFile()){
+        const targetDir= path.join(dirPath,ext)
+
+        await fs.mkdir(targetDir, { recursive: true } )
+
+
+        const targetPath=path.join(targetDir,item)
+
+        await fs.rename(filePath,targetPath)
+
+        console.log(`moved ${item} to ${targetPath}`)
+    }
+
+}
